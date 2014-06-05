b8972dd3c1ffaca76d27100fc0feacb9ad32b7b0
Patrick Boos <patrick@tutti.ch>
2014-06-05 07:53:05 +0200

    Added license

--- library/src/main/java/ch/tutti/android/applover/AppLoverDialogFragment.java
+++ library/src/main/java/ch/tutti/android/applover/AppLoverDialogFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 tutti.ch AG
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 package ch.tutti.android.applover;
 
 import android.annotation.TargetApi;
@@ -8,7 +23,8 @@ import android.os.Build;
 import android.os.Bundle;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class AppLoverDialogFragment extends DialogFragment implements AppLoverDialogHelper.DialogListener {
+public class AppLoverDialogFragment extends DialogFragment
+        implements AppLoverDialogHelper.DialogListener {
 
     public static AppLoverDialogFragment newInstance(int dialogType, int appNameResId) {
         AppLoverDialogFragment fragment = new AppLoverDialogFragment();
@@ -40,8 +56,8 @@ public class AppLoverDialogFragment extends DialogFragment implements AppLoverDi
     @Override
     public void onCancel(DialogInterface dialog) {
         super.onCancel(dialog);
-        AppLover.get(null)
-                .trackDialogCanceled(getArguments().getInt(AppLoverDialogHelper.ARGUMENT_DIALOG_TYPE));
+        AppLover.get(null).trackDialogCanceled(
+                getArguments().getInt(AppLoverDialogHelper.ARGUMENT_DIALOG_TYPE));
         AppLoverDialogHelper.onCancel(getActivity());
     }
 }
--- library/src/main/java/ch/tutti/android/applover/criteria/AppLoverAllCustomEventsReached.java
+++ library/src/main/java/ch/tutti/android/applover/criteria/AppLoverAllCustomEventsReached.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 tutti.ch AG
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 package ch.tutti.android.applover.criteria;
 
 import android.content.Context;
@@ -13,7 +28,8 @@ public class AppLoverAllCustomEventsReached implements AppLoverCriteria {
     @Override
     public boolean isCriteriaMet(Context context, AppLover appLover,
             AppLoverPreferences preferences) {
-        HashMap<String, Integer> mCustomEventThresholdMap = appLover.getCustomEventCountThresholds();
+        HashMap<String, Integer> mCustomEventThresholdMap =
+                appLover.getCustomEventCountThresholds();
         for (Map.Entry<String, Integer> thresholdEntry : mCustomEventThresholdMap.entrySet()) {
             Integer eventCount = preferences.getCustomEventCount(thresholdEntry.getKey());
             if (thresholdEntry.getValue() > eventCount) {
